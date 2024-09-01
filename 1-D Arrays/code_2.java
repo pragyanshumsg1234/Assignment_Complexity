c7e9d3852c949636c0ccb338f8f385eedc01c0a1
pragyanshumsg1234 <66196958+pragyanshumsg1234@users.noreply.github.com>
2024-09-01 11:32:11 +0530

    Add files via upload

---
+++ 1-D Arrays/code_2.java
@@ -0,0 +1,9 @@
+public class code_2 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        for (int elem : arr) {
+            if (elem % 2 == 0)
+                System.out.println(elem);
+        }
+    }
+}
---
+++ 1-D Arrays/code_3.java
@@ -0,0 +1,10 @@
+public class code_3 {
+    public static void main(String[] args) {
+        int[] arr = { 10, 7, -5, 8, 9, 0, -4 };
+        int max = Integer.MIN_VALUE;
+        for (int val : arr) {
+            max = Math.max(max, val);
+        }
+        System.out.print("Largest in given array is " + max);
+    }
+}
